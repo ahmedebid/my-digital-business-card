2b4c9c7a822473e35b25ebc1d20a1716b3d16b13
ahmedebid <ahmed-ebid@outlook.com>
2022-06-16 21:31:41 +0200

    Add the final version of the business card
    This update adds new content and CSS styles to reach the final version
    of the business card according to the Figma design file.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,6 @@
 import profilePic from "../images/profile-pic.png";
+import mailIcon from "../images/mail-icon.png";
+import linkedinIcon from "../images/linkedin-icon.png";
 
 export default function Info() {
     return (
@@ -7,8 +9,8 @@ export default function Info() {
             <h1>Ahmed Ebid</h1>
             <h3>Frontend Engineer</h3>
             <p>ebid.dev</p>
-            <button>Email</button>
-            <button>LinkedIn</button>
+            <button className="email-button"><img src={mailIcon} alt="mail icon"/>Email</button>
+            <button className="linkedin-button"><img src={linkedinIcon} alt="LinkedIn icon"/>LinkedIn</button>
         </header>
     )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <section className="interests-sec">
             <h2>Interests</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>Engineer. Academic. Reader. Internet fanatic. Coffee fanatic. Always Exploring.</p>
         </section>
     )
 }
